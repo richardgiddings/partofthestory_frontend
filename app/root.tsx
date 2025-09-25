c7ec9f6fff648aed875a018993a41143b802c879
Richard Giddings <rmpgiddings@gmail.com>
2025-09-25 09:46:29 +0100

    Remove type from links in root.tsx for css

--- app/root.tsx
+++ app/root.tsx
@@ -20,12 +20,10 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    type: "text/css",
   },
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Archivo+Black&family=Parisienne&display=swap",
-    type: "text/css",
   },
 ];
 
