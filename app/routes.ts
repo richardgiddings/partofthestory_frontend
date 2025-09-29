4bde970b502aaa85c15a54c2522672a39534e876
Richard Giddings <rmpgiddings@gmail.com>
2025-09-29 15:21:46 +0100

    Try adding an index route

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,12 @@
 import { 
     type RouteConfig, 
-    route 
+    route,
+    index
 } from "@react-router/dev/routes";
 
 export default [
-    route("/", "routes/home.tsx"),
+    index("routes/home.tsx"),
+    //route("/", "routes/home.tsx"),
     route("/about/", "routes/about.tsx"),
     route("/write/", "routes/write.tsx"),
     route("/my_stories/", "routes/my_stories.tsx")
