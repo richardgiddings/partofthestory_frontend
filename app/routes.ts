105fa6092d1c088152b09f293f373d75479bc545
Richard Giddings <rmpgiddings@gmail.com>
2025-09-29 16:12:04 +0100

    Playing with the index route again

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,7 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-    route("/", "routes/home.tsx"),
+    route("/index.html", "routes/home.tsx"),
     route("/about/", "routes/about.tsx"),
     route("/write/", "routes/write.tsx"),
     route("/my_stories/", "routes/my_stories.tsx")
