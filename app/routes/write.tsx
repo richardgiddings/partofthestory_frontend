f293bdcda0c95230131cf86c91a42a9ffc99eb36
Richard Giddings <rmpgiddings@gmail.com>
2025-10-22 15:55:38 +0100

    Move tour button to start of buttons

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -245,13 +245,13 @@ export default function Write({
 							</Card.Text>
 						</Card.Body>
 						<Card.Footer>
-							<NavLink to="/" end><Button className="me-1">Home</Button></NavLink>
-							<NavLink to="/my_stories" end><Button className="ms-1">My Stories</Button></NavLink>
-							<Button onClick={() => introJs.tour().start() } className="ms-2">
+							<Button onClick={() => introJs.tour().start() } className="me-1">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-circle-fill" viewBox="0 0 16 16">
 									<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
 								</svg>
-							</Button>			
+							</Button>
+							<NavLink to="/" end><Button className="me-1">Home</Button></NavLink>
+							<NavLink to="/my_stories" end><Button>My Stories</Button></NavLink>		
 						</Card.Footer>
 					</Card>
 				</Col>
