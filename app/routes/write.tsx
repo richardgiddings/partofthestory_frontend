3a9a4a031e3dba9b0432349c554d1e26826b5f86
Richard Giddings <rmpgiddings@gmail.com>
2026-03-30 17:07:18 +0100

    Menu alignment issues

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -180,17 +180,18 @@ export default function Write({
 					<Navbar>
 						<Nav>
 							<OverlayTrigger placement="top" overlay={<Tooltip id="button-tooltip-page-home">Go to home page</Tooltip>}>
-								<Nav.Link href="/" className="pt-0">
-									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
+								<Nav.Link href="/">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door-fill icon" viewBox="0 0 16 16">
 										<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
 									</svg>
-									<span className="button_text">Home</span>
+									Home
 								</Nav.Link>
 							</OverlayTrigger>
-							|
+						</Nav> |
+						<Nav>
 							<OverlayTrigger placement="top" overlay={<Tooltip id="button-tooltip-page-tour">What's on this page?</Tooltip>}>
-								<Nav.Link href="" onClick={() => introJs.tour().start() } className="pt-0">
-									<span className="button_text">Take a Tour</span>
+								<Nav.Link href="" onClick={() => introJs.tour().start() }>
+									Take a Tour
 								</Nav.Link>
 							</OverlayTrigger>	
 						</Nav>
